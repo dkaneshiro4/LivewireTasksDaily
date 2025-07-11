1cd963bc33a0d873008b9eaac48f9fd172a073a8
Kaneshiro, David <dkaneshiro4@honolulu.gov>
2025-07-10 16:18:03 -1000

    feat: implement task edit functionality with form validation

--- app/Livewire/Tasks/Edit.php
+++ app/Livewire/Tasks/Edit.php
@@ -2,11 +2,43 @@
 
 namespace App\Livewire\Tasks;
 
+use App\Models\Task;
 use Livewire\Component;
+use Illuminate\View\View;
+use Livewire\Attributes\Validate;
 
 class Edit extends Component
 {
-    public function render()
+    #[Validate('required|string|max:255')]
+    public string $name;
+
+    #[Validate('nullable|boolean')]
+    public bool $is_completed;
+
+    public Task $task;
+
+    public function mount(Task $task): void
+    {
+        $this->task = $task;
+        $this->name = $task->name;
+        $this->is_completed = $task->is_completed;
+    }
+
+    public function save(): void
+    {
+        $this->validate();
+
+        $this->task->update([
+            'name'         => $this->name,
+            'is_completed' => $this->is_completed,
+        ]);
+
+        session()->flash('success', 'Task successfully updated.');
+
+        $this->redirectRoute('tasks.index', navigate: true);
+    }
+
+    public function render(): View
     {
         return view('livewire.tasks.edit');
     }
--- resources/views/livewire/tasks/edit.blade.php
+++ resources/views/livewire/tasks/edit.blade.php
@@ -1,3 +1,20 @@
-<div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
-</div>
+<section class="max-w-5xl">
+    <form wire:submit="save" class="flex flex-col gap-6">
+        <flux:input
+            wire:model="name"
+            :label="__('Task Name')"
+            required
+            badge="required"
+        />
+
+        <flux:switch
+            wire:model="is_completed"
+            label="Completed?"
+            align="left"
+        />
+
+        <div>
+            <flux:button variant="primary" type="submit">{{ __('Save') }}</flux:button>
+        </div>
+    </form>
+</section>
